cd4a5874d934ddb1fe7b472b42d1d96325c47f45
Matthew Crawford <Tron1978@outlook.com>
2020-05-29 08:44:19 +0700

    updated all references to latest packages

--- src/BLTS.WebUi.Web.Mvc/Startup/Startup.cs
+++ src/BLTS.WebUi.Web.Mvc/Startup/Startup.cs
@@ -22,79 +22,79 @@ using Newtonsoft.Json.Serialization;
 
 namespace BLTS.WebUi.Web.Startup
 {
-    public class Startup
+  public class Startup
+  {
+    private readonly IConfigurationRoot _appConfiguration;
+
+    public Startup(IWebHostEnvironment env)
     {
-        private readonly IConfigurationRoot _appConfiguration;
-
-        public Startup(IWebHostEnvironment env)
-        {
-            _appConfiguration = env.GetAppConfiguration();
-        }
-
-        public IServiceProvider ConfigureServices(IServiceCollection services)
-        {
-            // MVC
-            services.AddControllersWithViews(
-                    options =>
-                    {
-                        options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
-                        options.Filters.Add(new AbpAutoValidateAntiforgeryTokenAttribute());
-                    }
-                )
-                .AddRazorRuntimeCompilation()
-                .AddNewtonsoftJson(options =>
-                {
-                    options.SerializerSettings.ContractResolver = new AbpMvcContractResolver(IocManager.Instance)
-                    {
-                        NamingStrategy = new CamelCaseNamingStrategy()
-                    };
-                });
-
-            IdentityRegistrar.Register(services);
-            AuthConfigurer.Configure(services, _appConfiguration);
-
-            services.AddScoped<IWebResourceManager, WebResourceManager>();
-
-            services.AddSignalR();
-
-            // Configure Abp and Dependency Injection
-            return services.AddAbp<WebUiWebMvcModule>(
-                // Configure Log4Net logging
-                options => options.IocManager.IocContainer.AddFacility<LoggingFacility>(
-                    f => f.UseAbpLog4Net().WithConfig("log4net.config")
-                )
-            );
-        }
-
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
-        {
-            app.UseAbp(); // Initializes ABP framework.
-
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-            }
-
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthentication();
-
-            app.UseJwtTokenMiddleware();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapHub<AbpCommonHub>("/signalr");
-                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
-                endpoints.MapControllerRoute("defaultWithArea", "{area}/{controller=Home}/{action=Index}/{id?}");
-            });
-        }
+      _appConfiguration = env.GetAppConfiguration();
     }
+
+    public IServiceProvider ConfigureServices(IServiceCollection services)
+    {
+      // MVC
+      services.AddControllersWithViews(
+              options =>
+              {
+                options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
+                options.Filters.Add(new AbpAutoValidateAntiforgeryTokenAttribute());
+              }
+          )
+          .AddRazorRuntimeCompilation()
+          .AddNewtonsoftJson(options =>
+          {
+            options.SerializerSettings.ContractResolver = new AbpMvcContractResolver(IocManager.Instance)
+            {
+              NamingStrategy = new CamelCaseNamingStrategy()
+            };
+          });
+
+      IdentityRegistrar.Register(services);
+      AuthConfigurer.Configure(services, _appConfiguration);
+
+      services.AddScoped<IWebResourceManager, WebResourceManager>();
+
+      services.AddSignalR();
+
+      // Configure Abp and Dependency Injection
+      return services.AddAbp<WebUiWebMvcModule>(
+          // Configure Log4Net logging
+          options => options.IocManager.IocContainer.AddFacility<LoggingFacility>(
+              f => f.UseAbpLog4Net().WithConfig("log4net.config")
+          )
+      );
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
+    {
+      app.UseAbp(); // Initializes ABP framework.
+
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+      else
+      {
+        app.UseExceptionHandler("/Error");
+      }
+
+      app.UseStaticFiles();
+
+      app.UseRouting();
+
+      app.UseAuthentication();
+
+      app.UseJwtTokenMiddleware();
+
+      app.UseAuthorization();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapHub<AbpCommonHub>("/signalr");
+        endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+        endpoints.MapControllerRoute("defaultWithArea", "{area}/{controller=Home}/{action=Index}/{id?}");
+      });
+    }
+  }
 }
